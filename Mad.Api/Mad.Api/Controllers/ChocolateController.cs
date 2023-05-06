fd42573a566a5fec1938e1adeade682298b41505
Bradamar Longbottom - Владислав Пугачев <bradamar@student.21-school.ru>
2023-05-06 07:59:05 +0300

    Первый этап "запуска работы с шоколадной индустрией") Методы для компаний и брендов: Создание, отображение списка всех существующих компаний или брендов, поиск определенной компании или бренда (по названию только на данный момент) Без SQL пока не знаю как именно правильно сделать "базу данных на одну сессию", поэтому сделал static классы пока что. Интересно будет получить в дальнейшем совет, как можно хранить данные в "долгой" перспективе без SQL например) (чисто ради интереса, так бы лучше использовал SQL) В планах еще добавить зависимости бренд -> компания, реализую добавление продуктов и аналогичная зависимость продукт -> бренд и т.п. В контроллере только вызовы сервисов, ничего лишнего.

---
+++ Mad.Api/Mad.Api/Controllers/ChocolateController.cs
@@ -0,0 +1,43 @@
+﻿using Mad.Bl.Services.Chocolate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mad.Api.Controllers
+{
+    [ApiController]
+    [Route("chocolate")]
+    public class ChocolateController : ControllerBase
+    {
+        private readonly ChocolateService serviceEntity = new();
+
+        [HttpPut("create-company")]
+        public IActionResult CreateCompany(string companyName) 
+        {
+            return Ok(serviceEntity.NewCompany(companyName));
+        }
+        [HttpGet("list-of-companies")]
+        public IActionResult GetCompanies() 
+        {
+            return Ok(serviceEntity.GetCompanies());
+        }
+        [HttpGet("search-company")]
+        public IActionResult SearchCompany(string companyName)
+        {
+            return Ok(serviceEntity.SearchCompany(companyName));
+        }
+        [HttpPut("create-brand")]
+        public IActionResult CreateBrand(string brandName)
+        {
+            return Ok(serviceEntity.NewBrand(brandName));
+        }
+        [HttpGet("list-of-brands")]
+        public IActionResult GetBrands()
+        {
+            return Ok(serviceEntity.GetBrands());
+        }
+        [HttpGet("search-brand")]
+        public IActionResult SearchBrand(string brandName)
+        {
+            return Ok(serviceEntity.SearchBrand(brandName));
+        }
+    }
+}
---
+++ Mad.Api/Mad.Data/Entities/Chocolate/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace Mad.Data.Entities.Chocolate
+{
+    public static class Brand
+    {
+        public static List<Guid> Id = new();
+        public static List<string> Name = new();
+    }
+}
